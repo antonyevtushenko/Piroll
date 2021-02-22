5846ad745ea144dc6ebb6de310ed4cdb2de1970f
Anton Yevtushenko <antonyevtushenko@MacBook-Pro-Anton.local>
2021-02-22 12:18:04 +0200

    changed swiper slider on click but still doesn't work

--- app.js
+++ app.js
@@ -1,23 +1,4 @@
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-      },
-    
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    
-      // And if we need scrollbar
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      },
-    });
-
-    let modal = document.querySelector(".modal")
+let modal = document.querySelector(".modal")
 let closeBtn = document.querySelector(".close-btn")
 
 let scrollPosition = 0;
@@ -51,4 +32,11 @@ modal.addEventListener("click", function(e){
     }
 })
 
+$(".your-class").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+});
 
